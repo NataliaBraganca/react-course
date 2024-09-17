f1a70da83ae9feb4494459fa5c64f3d48fddc411
Natalia Eifländer Braganca <nataliabragancan@gmail.com>
2024-09-17 18:39:57 +0200

    gitignore and setup react project

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 
 
-createRoot(document.getElementById('root')).render( <App />)
+ReactDOM.createRoot(document.getElementById('root')).render( <App />)
