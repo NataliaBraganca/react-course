c2c13fdb464aefa9cec70aafea524032f79cbac0
Natalia Eifländer Braganca <nataliabragancan@gmail.com>
2024-09-20 15:23:19 +0200

    Hook

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,16 @@ import Footer from './Footer'
 import Form from "./Form/Form"
 
 const App = () => {
+  const [ativo, setAtivo] = React.useState(true);
+  // É a mesma coisa que:
+  // const ativoArray = React.useState(true);
+  // const ativo = ativoArray[0];
+  // const setAtivo = ativoArray[1];
+
   return (
-    <div>
-      
-      <Form />
-      <Footer />
-    </div>
+    <button onClick={() => setAtivo(!ativo)}>
+      {ativo ? 'Botão Ativo' : 'Botão Inativo'}
+    </button>
   );
 };
 
