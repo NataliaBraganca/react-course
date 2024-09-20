32e22cc9cb72959706ffc8fff57b79db0da224b1
Natalia Eifländer Braganca <nataliabragancan@gmail.com>
2024-09-20 15:41:18 +0200

    add new props object

--- src/App.jsx
+++ src/App.jsx
@@ -3,16 +3,26 @@ import Footer from './Footer'
 import Form from "./Form/Form"
 
 const App = () => {
-  const [ativo, setAtivo] = React.useState(true);
+  const [ativo, setAtivo] = React.useState(false);
+  const [dados, setDados] = React.useState({nome: "Andre", idade: "30"});
   // É a mesma coisa que:
   // const ativoArray = React.useState(true);
   // const ativo = ativoArray[0];
   // const setAtivo = ativoArray[1];
 
+  function handleClick(){
+    setAtivo(!ativo);
+    setDados({...dados, faculdade: "Possui faculdade"})
+
+  }
+
   return (
-    <button onClick={() => setAtivo(!ativo)}>
-      {ativo ? 'Botão Ativo' : 'Botão Inativo'}
-    </button>
+    <div>
+      <p>{dados.nome}</p>
+      <p>{dados.idade}</p>
+      <p>{dados.faculdade}</p>
+      <button onClick={handleClick}>{ativo ? 'Botão Ativo' : 'Botão Inativo'}</button>
+    </div>
   );
 };
 
